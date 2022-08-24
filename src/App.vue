cecfd08b8e215c87c78d1269fe58691d3b427b7a
Eddie-Sc <scales.ed10@hotmail.co.nz>
2022-08-24 21:25:31 +1200

    Team2Edit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-<h2> profile count: {{profiles.length}}</h2>
 
-<input type="text" v-model.trim="id"> {{id}}
+<div id="header">
+
+  <h1>Protégé</h1>
+<h3> profile count: {{profiles.length}}</h3>
+
+<input type="text" v-model.trim="id"> 
+<!-- {{id}} --> 
+<br>
 <br>
 
 <input type="text" placeholder="username" v-model="formValues.username">
@@ -9,6 +15,7 @@
 <input type="text" placeholder="ImageUrl" v-model="formValues.imageUrl">
 
 
+<br>
 <br>
 <button @click="insertDoc"> Insert student </button>
 
@@ -20,26 +27,31 @@
 
 <br>
 
-
+</div>
 
 <ul>
-  <li v-for="profile in profiles">
+  <li v-for="profile in profiles" id="flex-cont">
   <img :src="profile.imageUrl" alt="" width="50">
-  {{profile.imageUrl}}
-   {"id is here" {{profile._id}} }
-  {{profile.username}}
-  {{profile.email}}
+<br>
+  id# {{profile._id}} 
+  <br>
+  User name: {{profile.username}}
+  <br>
+  Email: {{profile.email}}
+  <hr>
   </li>
 </ul>
 
+<hr>
 
 
 </template>
 
 
 <script>
-const api ="https://rest-api-express-mongodb.netlify.app/.netlify/functions/api/"
-// const api = "https://rest-api-jameseddievictoria.netlify.app/.netlify/functions/api/"
+// const api ="https://rest-api-express-mongodb.netlify.app/.netlify/functions/api/"
+const api = "https://rest-api-jameseddievictoria.netlify.app/.netlify/functions/api/"
+
 export default{
   data(){
     return{
@@ -139,15 +151,104 @@ mounted(){
 
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+
+
+ body {
+        font-family: 'Roboto', sans-serif;
+        text-align: center;   
+    }
+ 
+    #headertext {
+        display: flex;
+        flex-direction: column;
+    }
+    #logoimg {
+        width: 250px;
+        height: auto;
+        margin: 20px;
+    }
+    #headertext {
+        margin-right: 20px;
+        margin-bottom: 0px;
+        justify-content: center;
+    }
+   
+img{border-radius: 50%;
+height: 10vw;
+width: 10vw;
+margin-right: 5vw;}
+
+#flex-cont{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-content: space-between;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+
+
+li{
+  padding-top: 20px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+
+   * {
+        font-family: Roboto, sans-serif;
+        margin: 0;
+    }
+    #header {
+        background-image: linear-gradient(#36C5C8, #3192D1);
+        height: 30%;
+        width: 100%;
+        margin: auto 0;
+        padding: 20px;
+    }
+  
+    p {
+        line-height: 1.3rem;
+    }
+    h1 {
+        font-size: 100px;
+        font-weight: 900;
+        margin: 0;
+        color: white;
+    }
+    h2 {
+        font-size: 40px;
+        font-style: italic;
+        margin: 0;
+        color: white;
+    }
+    h3 {
+        font-weight: 700;
+        font-size: 1.4rem;
+        margin: 10px 0 20px 0;
+        color: white;
+    }
+input{border: none;
+        padding: 6px 10px;
+        background-color: #a5a5a5;
+        border-radius: 20px;
+          font-weight: 600;
+        font-size: 0.8rem;
+        margin: 10px;
+        color: #ffffff;}
+
+
+button {
+        border: rgb(255, 255, 255) solid 2px;
+        padding: 6px 10px;
+        background-color: #3192D1;
+        border-radius: 20px;
+        color: #ffffff;
+        font-weight: 600;
+        font-size: 0.8rem;
+        margin: 10px;
+    }
+    button:hover {
+        background-color: #ffffff;
+        color: #3192D1;
+        border: rgb(255, 255, 255) solid 2px;
+    }
+
 </style>
